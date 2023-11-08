af4c8754af07268193a420cec127b15891cc7379
thuolala <phamthushame2002@gmail.com>
2023-11-08 12:28:58 +0700

    add docs

--- 52000409_52000574.py
+++ 52000409_52000574.py
@@ -89,7 +89,7 @@ cv.imwrite("purple_star.png", res)
 # 1b) 
 # Read input img as grayscale
 input1_gray = cv.imread("input1.jpg", cv.IMREAD_GRAYSCALE) 
-# cv.imshow('Grayscale', input1_gray)
+cv.imshow('Grayscale', input1_gray)
 
 # Change white borders to black borders 
 th, dst = cv.threshold(input1_gray, 220, 0, cv.THRESH_TOZERO_INV)
@@ -116,81 +116,80 @@ cv.destroyAllWindows()
 
 # ------------------------------------------------------------------------------------------------------- #
 
-# # TASK 2 
+# TASK 2 
 
-# # Read input img as grayscale
-# input2 = cv.imread("input2.png", cv.IMREAD_GRAYSCALE)  
-# height, width = input2.shape
+# Read input img as grayscale
+input2 = cv.imread("input2.png", cv.IMREAD_GRAYSCALE)  
+height, width = input2.shape
 
 
-# # ----------------------Extract only area with a lot of noise then pre-processing it----------------------
-# # Blur
-# blur_noise = cv.medianBlur(input2, 11) 
+# ----------------------Extract only area with a lot of noise then pre-processing it----------------------
+# Blur
+blur_noise = cv.medianBlur(input2, 11) 
 
-# # Threshold
-# th1 = cv.adaptiveThreshold(blur_noise, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
-# ret, th2 = cv.threshold(th1, 0, 255, cv.THRESH_BINARY_INV)
+# Threshold
+th1 = cv.adaptiveThreshold(blur_noise, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+ret, th2 = cv.threshold(th1, 0, 255, cv.THRESH_BINARY_INV)
 
-# img_lotNoise = th2[270:height, 260:width]  
+img_lotNoise = th2[270:height, 260:width]  
 
-# # Pre-processing that area 
-# result = cv.erode(img_lotNoise, cv.getStructuringElement(cv.MORPH_CROSS, (3, 6)), iterations=1)
-# result = cv.erode(result, cv.getStructuringElement(cv.MORPH_RECT, (1, 2)), iterations=2)
-# result = cv.dilate(result, cv.getStructuringElement(cv.MORPH_CROSS, (1, 2)), iterations=1)
-# result = cv.morphologyEx(result, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_RECT, (4, 7)))
+# Pre-processing that area 
+result = cv.erode(img_lotNoise, cv.getStructuringElement(cv.MORPH_CROSS, (3, 6)), iterations=1)
+result = cv.erode(result, cv.getStructuringElement(cv.MORPH_RECT, (1, 2)), iterations=2)
+result = cv.dilate(result, cv.getStructuringElement(cv.MORPH_CROSS, (1, 2)), iterations=1)
+result = cv.morphologyEx(result, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_RECT, (4, 7)))
 
-# # -----------------------------------Pre-processing the original image-----------------------------------
-# # Blur
-# blur = cv.medianBlur(input2, 7) 
+# -----------------------------------Pre-processing the original image-----------------------------------
+# Blur
+blur = cv.medianBlur(input2, 7) 
 
-# # Otsu threshold
-# thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+# Otsu threshold
+thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
 
-# # Opening
-# kernel1 = cv.getStructuringElement(cv.MORPH_RECT, (4,3))
-# opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel1)
+# Opening
+kernel1 = cv.getStructuringElement(cv.MORPH_RECT, (4,3))
+opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel1)
 
-# # Re-attacht that area with original 
-# ret, result = cv.threshold(result, 0, 255, cv.THRESH_BINARY_INV)
-# opening[270:height, 260:width] = result
+# Re-attach that area with original 
+ret, result = cv.threshold(result, 0, 255, cv.THRESH_BINARY_INV)
+opening[270:height, 260:width] = result
 
-# # Display it
-# # cv.imshow("After Cleaning Up", opening)
+# Display it
+cv.imshow("After Cleaning Up", opening)
 
-# # ------------------------------------------Draw contours-------------------------------------------
-# # Contour 
-# img = cv.imread("input2.png")
-# ret, thresh = cv.threshold(opening, 0, 255, cv.THRESH_BINARY_INV)
+# ------------------------------------------Draw contours-------------------------------------------
+# Contour 
+img = cv.imread("input2.png")
+ret, thresh = cv.threshold(opening, 0, 255, cv.THRESH_BINARY_INV)
 
-# # Detect contours
-# contours, hie = cv.findContours(image=thresh, mode=cv.RETR_EXTERNAL, method=cv.CHAIN_APPROX_SIMPLE) 
+# Detect contours
+contours, hie = cv.findContours(image=thresh, mode=cv.RETR_EXTERNAL, method=cv.CHAIN_APPROX_SIMPLE) 
 
-# # Draw contours
-# img_copy = img.copy() 
+# Draw contours
+img_copy = img.copy() 
 
-# # Get sum of h & w 
-# count = 0  
-# sum_h = 0 
-# sum_w = 0 
-# for cnt in contours:
-#     x,y,w,h = cv.boundingRect(cnt) 
-#     sum_h += h 
-#     sum_w += w 
-#     count += 1 
+# Get sum of h & w 
+count = 0  
+sum_h = 0 
+sum_w = 0 
+for cnt in contours:
+    x,y,w,h = cv.boundingRect(cnt) 
+    sum_h += h 
+    sum_w += w 
+    count += 1 
 
-# # Get mean of h & w 
-# mean_h = sum_h / count 
-# mean_w = sum_w / count 
+# Get mean of h & w 
+mean_h = sum_h / count 
+mean_w = sum_w / count 
 
-# for cnt in contours:
-#     # R = random.randint(0, 255)
-#     # G = random.randint(0, 255)
-#     # B = random.randint(0, 255)
-#     x,y,w,h = cv.boundingRect(cnt)
-#     if (h >= mean_h):
-#         img_copy = cv.rectangle(img_copy,(x,y),(x+w,y+h),(0, 255, 0),2)
-# cv.imwrite("output2.png", img_copy)
-# cv.imshow("Drawed Contours Image", img_copy)
-
-# cv.waitKey(0)
-# cv.destroyAllWindows()
+for cnt in contours:
+    # R = random.randint(0, 255)
+    # G = random.randint(0, 255)
+    # B = random.randint(0, 255)
+    x,y,w,h = cv.boundingRect(cnt)
+    if (h >= mean_h):
+        img_copy = cv.rectangle(img_copy,(x,y),(x+w,y+h),(0, 255, 0),2)
+cv.imwrite("output2.png", img_copy)
+cv.imshow("Drawed Contours Image", img_copy)
+cv.waitKey(0)
+cv.destroyAllWindows()
